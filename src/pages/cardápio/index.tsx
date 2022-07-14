41cae112341cafd7e2f3b406a0312f416acafea2
Roger! <arogerlima@gmail.com>
2022-07-14 17:13:31 -0300

    tenth commit

--- src/pages/cardápio/index.tsx
+++ src/pages/cardápio/index.tsx
@@ -1,12 +1,12 @@
 import styles from "./Cardapio.module.scss";
-import { ReactComponent as Logo} from 'assets/logo.svg'
-import Buscador from "./buscador";
+import { ReactComponent as Logo } from "assets/logo.svg";
+import Buscador from "./Buscador";
 import { useState } from "react";
-import Filtros from "./filtros";
+import Filtros from "./Filtros";
 
 export default function Cardapio() {
   const [busca, setBusca] = useState("");
-
+  const [filtro, setFiltro] = useState<number | null>(null);
   return (
     <main>
       <nav className={styles.menu}>
@@ -14,14 +14,14 @@ export default function Cardapio() {
       </nav>
       <header className={styles.header}>
         <div className={styles.header__text}>
-          A casa do código e da massa 
+          A casa do código e da massa
         </div>
       </header>
       <section className={styles.cardapio}>
         <h3 className={styles.cardapio__titulo}>Cardápio</h3>
-        <Buscador busca={busca} setBusca={setBusca}/>
+        <Buscador busca={busca} setBusca={setBusca} />
         <div className={styles.cardapio__filtros}>
-          <Filtros />
+          <Filtros filtro={filtro} setFiltro={setFiltro} />
         </div>
       </section>
     </main>
